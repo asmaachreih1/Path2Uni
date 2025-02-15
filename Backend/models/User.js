93df6c5e3cadf57521660f853dcb59bef25695ed
leen938 <lmf17@mail.aub.edu>
2025-02-15 21:08:22 +0200

    Removed incorrect User.js file

--- Backend/models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-//to create a user schema
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },//store the user's email which must be unique
-  password: { type: String, required: true },//store the user's hashed pass
-});
