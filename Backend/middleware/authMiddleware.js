4373a110f448da7c553c0e9efd02416abbf29633
leen938 <lmf17@mail.aub.edu>
2025-02-15 21:56:29 +0200

    Re-added updated files

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 
 exports.authMiddleware = (req, res, next) => {
   const token = req.header('Authorization');
+  //retrieves JWT token from request headers
+  //if no token is found it returns error 401 which means it is unauthorized
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
