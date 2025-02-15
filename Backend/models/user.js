b86cc59eb9f3cf6165bfd5dbbf49697f315b37f6
leen938 <lmf17@mail.aub.edu>
2025-02-15 20:07:16 +0200

    Updated User.js with password hashing

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,11 +1,14 @@
-//omar
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    resetToken: { type: String },
-    resetTokenExpiry: { type: Date }
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+//to create a user schema
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },//store the user's email which must be unique
+  password: { type: String, required: true },//store the user's hashed pass
 });
-
-module.exports = mongoose.model("User", userSchema);
+//hash password before saving for security reasons using bcrypt
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
