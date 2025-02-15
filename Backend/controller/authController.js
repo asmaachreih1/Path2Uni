1a982fa1fad7dd3b009c033a15d0eb883b91c360
leen938 <lmf17@mail.aub.edu>
2025-02-15 21:38:30 +0200

    Re-added updated files

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -4,6 +4,40 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const sendResetEmail = require("../utils/mailer");
 
+//leen
+const jwt= require("jsonwentoken");//generate authentication tokens
+
+//sign in handler
+//searches for the user in the database using email
+//if the email doesn’t exist it returns error 400
+//if the email exists it compares the entered pass with the stored hashed pass
+exports.signIn = async (req, res) => { 
+    try {
+        const { email, password } = req.body;
+
+        //check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        //validate password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        //creates a JWT token using the user's ID
+        //returns the token in the response for the frontend to use
+        const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+
+        res.json({ token, message: "Sign in successful" });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+//omar
 // Forgot Password Handler
 exports.forgotPassword = async (req, res) => {
     const { email } = req.body;
