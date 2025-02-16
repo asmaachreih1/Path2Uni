a32ecd69ffe6d6564d1223a489f3cd43dad7174d
Safadiii <ASAFADI21@ku.edu.tr>
2025-02-16 11:14:39 +0200

    changes

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -3,5 +3,10 @@ const nodemailer = require("nodemailer");
 require("donetv").config();
 
 const sendResetEmail = async (email, token) => {
-    
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+    });
+
+
+
 }
