3a02a0c09f038f68c131d1be7291f5e3b4400350
Safadiii <ASAFADI21@ku.edu.tr>
2025-02-16 13:30:48 +0200

    Updated user.js code

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,1 +1,19 @@
 //omar 16/2/2025
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
