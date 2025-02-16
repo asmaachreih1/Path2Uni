d8957544f80894a42b4d1eff273a10c6352343f8
Safadiii <ASAFADI21@ku.edu.tr>
2025-02-16 11:19:30 +0200

    mailer.js forgetpass

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -10,7 +10,14 @@ const sendResetEmail = async (email, token) => {
             pass: process.env.EMAIL_PASS
         }
     });
+const mailOptions = {
+        to: email,
+        from: process.env.EMAIL_USER,
+        subject: "Password Reset Request",
+        html: `<p>You requested a password reset</p>
+               <p>Click <a href="http://localhost:3000/reset-password/${token}">here</a> to reset your password</p>`
+    };
 
-
-
-}
+    await transporter.sendMail(mailOptions);
+};
+module.exports = sendResetEmail;
