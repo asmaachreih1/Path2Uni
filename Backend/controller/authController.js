2d55d7e8aedfd4376216efa44919750a7f8586f1
Safadiii <ASAFADI21@ku.edu.tr>
2025-02-16 13:42:58 +0200

    forget pass

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -11,6 +11,10 @@ exports.forgotPassword = async (req, res) => {
 
         if (!user) return res.status(404).json({ message: "User not found" });
 
+        const resetToken = crypto.randomBytes(32).toString('hex');
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        await user.save();
 
 
 //leen
