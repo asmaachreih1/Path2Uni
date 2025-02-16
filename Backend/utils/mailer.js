6572528e9699e968380cc2b977e7a52d70798500
Safadiii <ASAFADI21@ku.edu.tr>
2025-02-16 11:08:44 +0200

    backend forget pass

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -1,25 +1,3 @@
 //omar
 const nodemailer = require("nodemailer");
-require("dotenv").config();
-
-const sendResetEmail = async (email, token) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    const mailOptions = {
-        to: email,
-        from: process.env.EMAIL_USER,
-        subject: "Password Reset Request",
-        html: `<p>You requested a password reset</p>
-               <p>Click <a href="http://localhost:3000/reset-password/${token}">here</a> to reset your password</p>`
-    };
-
-    await transporter.sendMail(mailOptions);
-};
-
-module.exports = sendResetEmail;
+require("donetv").config();
