36d585df6399fb8487b3f68e454ebb7eed38498a
Safadiii <ASAFADI21@ku.edu.tr>
2025-02-16 11:15:51 +0200

    fotgot pass

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -5,6 +5,10 @@ require("donetv").config();
 const sendResetEmail = async (email, token) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
     });
 
 
