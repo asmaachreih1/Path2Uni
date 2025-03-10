1fe39384375290f171d1eacc33f5d3fd09e2e1cf
ctan06 <cleopatre.tan@gmail.com>
2025-03-10 19:00:49 +0200

    Login js code

--- Frontend/feastures/login/login.js
+++ Frontend/feastures/login/login.js
@@ -55,3 +55,20 @@ loginForm.addEventListener('submit', async function (event) {
         console.error("Backend Error:", errorData);
     }
 });
+
+function togglePassword() {
+    const passwordField = document.getElementById('password');
+    const toggleIcon = document.querySelector('.toggle-password i');
+
+    // Toggle the input type between password and text
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash'); // Change icon to "eye-slash" (hide password)
+    } else {
+        passwordField.type = 'password';
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye'); // Change icon back to "eye" (show password)
+    }
+}
+
