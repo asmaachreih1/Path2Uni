9b853188ee7c2423af25416cca37284ec55f2314
Nour Sakr <nhs32@mail.aub.edu>
2025-03-16 00:01:55 +0200

    scholarships frontend (js)

--- Frontend/feastures/scholarships/scholarships.js
+++ Frontend/feastures/scholarships/scholarships.js
@@ -1,2 +1,14 @@
 
 console.log("Scholarships page loaded!");
+
+// Add hover effect to scholarship boxes
+const scholarshipBoxes = document.querySelectorAll('.scholarship-box');
+
+scholarshipBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        box.style.transform = 'translateY(-5px)';
+        setTimeout(() => {
+            box.style.transform = 'translateY(0)';
+        }, 200);
+    });
+});
