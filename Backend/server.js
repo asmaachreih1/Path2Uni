5869d3832e1b8398e3200882dd79407b89c1ae69
Nour Sakr <nhs32@mail.aub.edu>
2025-03-15 21:32:04 +0200

    sign out, connecting to frontend (server.js)

--- Backend/server.js
+++ Backend/server.js
@@ -1,45 +1,19 @@
-const cors =require('cors');//l
-const express = require('express');
-require('dotenv').config();
+require('dotenv').config();  // This loads the .env file
 const mongoose = require('mongoose');
-
-
-//console.log("Loaded ENV:", process.env.MONGODB_URI); // Debugging
-
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5001;//l
 
 
-//  PROPERLY CONFIGURE CORS
-app.use(cors({
- // origin: 'http://127.0.0.1:5500', // Allow requests from Live Server
- origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
- //methods: ['GET', 'POST', 'PUT', 'DELETE'],
- methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
- //allowedHeaders: ['Content-Type', 'Authorization']
- allowedHeaders: "Content-Type,Authorization",
- credentials: true
-}));
-app.use(cors());//l
-
-//import Routes leen march 2
 const authRoutes = require('./routes/authRoutes');//l
-app.use('/api', authRoutes);//l
 
-
-
-
-// Serve static files from the Frontend directory
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'Frontend')));
-
-
+const cors =require('cors');//l
+app.use(cors());//l
 
 // Middleware to parse JSON
 app.use(express.json());//l
-//use Routes leen march 2
-
+app.use('/api', authRoutes);//l
 
 
 // Mongo URI from .env file
@@ -67,4 +41,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
