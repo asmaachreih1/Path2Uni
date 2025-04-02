40d35c0c9086bcd0641e51a8f40890b6abd75e7e
asmaachreih1 <136915042+asmaachreih1@users.noreply.github.com>
2025-04-02 17:13:46 +0300

    Update aub.js

--- Frontend/feastures/aub/aub.js
+++ Frontend/feastures/aub/aub.js
@@ -2,5 +2,6 @@ function toggleDetails(id) {
     var details = document.getElementById(id);
     if (details.style.display === "none" || details.style.display === "") {
         details.style.display = "block";
-   
+    } else {
+    
 
