3215f38f2ac4337d4e4dc2ca7bd8e8486def7650
leen938 <lmf17@mail.aub.edu>
2025-04-14 12:57:35 +0300

    Update Job.js

--- Backend/models/Job.js
+++ Backend/models/Job.js
@@ -4,7 +4,7 @@ const JobSchema = new mongoose.Schema({
     mentorName: { type: String, required: true },
     mentorField: { type: String, required: true },
     mentorBackground: { type: String, required: true },
-   
+    title: { type: String, required: true },
     content: { type: String, required: true },
     postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     createdAt: { type: Date, default: Date.now }
