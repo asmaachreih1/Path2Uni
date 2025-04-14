ca81581b47951eb0088ff524617b93b39ddfd227
leen938 <lmf17@mail.aub.edu>
2025-04-14 12:55:38 +0300

    Update userController.js

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -6,7 +6,5 @@ exports.getUserRole = async (req, res) => {
         if (!user) return res.status(404).json({ message: 'User not found' });
 
         res.json({ role: user.role });
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching user role', error });
-    }
+    } 
 };
