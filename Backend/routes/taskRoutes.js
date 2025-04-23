3460386907ee1f3df196518048a71cfefa479fed
asmaachreih1 <136915042+asmaachreih1@users.noreply.github.com>
2025-04-23 16:32:29 +0300

    Update taskRoutes.js

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,1 +1,16 @@
 
+const express = require('express');
+const router = express.Router();
+//const { getTasks, addTask, deleteTask, toggleTask } = require('../controller/taskController');
+const { authMiddleware } = require('../middleware/authMiddleware');
+const taskController = require('../controller/taskController');
+
+
+router.use(authMiddleware); // Protect all task routes
+
+router.get('/', authMiddleware, taskController.getTasks);
+router.post('/', authMiddleware, taskController.addTask);
+router.put('/toggle/:id', authMiddleware, taskController.toggleTask);
+router.delete('/:id', authMiddleware, taskController.deleteTask);
+
+module.exports = router;
