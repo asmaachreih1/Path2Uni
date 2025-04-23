5af0f2768104407bafef1238f91d386042de9892
asmaachreih1 <136915042+asmaachreih1@users.noreply.github.com>
2025-04-23 16:31:38 +0300

    Update Task.js

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  checked: {
+    type: Boolean,
+    default: false,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', TaskSchema);
 
