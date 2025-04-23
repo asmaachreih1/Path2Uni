86e384498d5b919a0b30762768f54a547c85544a
asmaachreih1 <136915042+asmaachreih1@users.noreply.github.com>
2025-04-23 19:39:50 +0300

    Create userController.js

---
+++ Backend/middleware/userController.js
@@ -0,0 +1,12 @@
+const User = require('../models/user');
+
+exports.getUserRole = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        res.json({ role: user.role });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching user role', error });
+    }
+};
