a144b68b3215f1a2d62bd380cbb6d8cc7d9d6ad1
asmaachreih1 <136915042+asmaachreih1@users.noreply.github.com>
2025-04-23 22:07:12 +0300

    Update tracking-list.js

--- Frontend/feastures/trackinglist/tracking-list.js
+++ Frontend/feastures/trackinglist/tracking-list.js
@@ -71,7 +71,7 @@ listContainer.addEventListener("click", async (e) => {
         });
         li.classList.toggle("checked");
     } else if (e.target.tagName === "SPAN") {
-        await fetch
+        await fetch(`${API_
 
             method: "DELETE",
             headers: { 'Authorization': `Bearer ${token}` }
